e9a6f3f292430e560e16405c0652b999134f09f4
Maxim Lee <maxim.lee@noveogroup.com>
2025-11-02 14:50:24 +0500

    Add open camera and saved photos grids

--- src/navigation/screens/Add.tsx
+++ src/navigation/screens/Add.tsx
@@ -1,10 +1,58 @@
 import { Text } from "@react-navigation/elements";
-import { StyleSheet, View } from "react-native";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
+import { useRef, useState } from "react";
+import { Button, FlatList, Image, StyleSheet, TouchableOpacity, View } from "react-native";
 
 export function Add() {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const [images, setImages] = useState<string[]>([]);
+  const cameraRef = useRef<CameraView>(null);
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title='grant permission' />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  async function takePicture() {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      if (photo) {
+        setImages((prevImages) => [photo.uri, ...prevImages]);
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Add Screen</Text>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={takePicture}>
+          <Text style={styles.text}>Take</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.galleryContainer}>
+        <FlatList
+          data={images}
+          horizontal
+          renderItem={({ item }) => <Image source={{ uri: item }} style={styles.image} />}
+          keyExtractor={(item) => item}
+        />
+      </View>
     </View>
   );
 }
@@ -13,7 +61,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 128,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    width: "100%",
+    paddingHorizontal: 64,
+    justifyContent: "center",
+  },
+  button: {
     alignItems: "center",
-    gap: 10,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  galleryContainer: {
+    position: "absolute",
+    bottom: 0,
+    height: 120,
+    width: "100%",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 5,
+    borderRadius: 10,
   },
 });
